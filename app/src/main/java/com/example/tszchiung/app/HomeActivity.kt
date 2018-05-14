3ce90a569d4cce72885ce380915e47cf6132da66
chunkitmax <chunkitmax1125@gmail.com>
2018-05-14 18:16:32 +0800

    Create Home Activity

---
+++ app/src/main/java/com/example/tszchiung/app/HomeActivity.kt
@@ -0,0 +1,31 @@
+package com.example.tszchiung.app
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
+    }
+}
